c0fa4b4c876e1e9fb7a9570a0c9f4acd6d1f515c
Vinícius Coelho <viniciusraposo@gmail.com>
2023-03-26 18:10:00 -0300

    api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
-  res.status(200).json({ text: 'Hello' });
+  res.status(200).json({ 
+    name: 'vinCoelho',
+    text: 'Hello, my friends.',
+    description: 'Just testing the endpoint',
+    beautifulArray: ['xablau', 'macacos me mordam']
+  });
 }
